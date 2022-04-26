19fd20ba833d5040fb09a429bf0912079cb52e3a
hutchcrafts <103635952+hutchcrafts@users.noreply.github.com>
2022-04-26 10:59:56 +0100

    Update

--- Activity5.sh
+++ Activity5.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+RED='\033[0;31m'                                        #red 
+BLUE='\033[0;34m'					#blue
+NC='\033[0m'						#no colour 
+
 while true; read -p "Enter a number: " num   
 
 do							#check if the input is numerical
-	echo "$num" | grep -q '[[:punct:]]' || echo "$num" | grep -iq [a-z] 			#-i for case insensitive; -q for search without results output
-	if test $? -eq 0 ; 
+	echo "$num" | grep -q '[[:punct:]]' || echo "$num" | grep -iq [a-z]  	#grep -i is for case insensitive; 
+										#grep -q for search without results output
+	if test $? -eq 0 ; 				#if input is incorrect
 	then
-		echo "This is not a number! Try again "
+		echo -e "${RED}This is not a number! Try again ${NC}"
 	continue					#go to entering input again
 	
 	else
-		echo $num | rev				#reversed output
+		echo -e "${BLUE}Your reversed number: ${NC}"
+		echo $num | rev				#display reversed output
 	fi
 	break						#break the loop
 done
-
