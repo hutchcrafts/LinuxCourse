9a8573025b8bfaf03b8434524697c36573faf578
hutchcrafts <103635952+hutchcrafts@users.noreply.github.com>
2022-04-26 11:00:31 +0100

    Update

--- Activity6.sh
+++ Activity6.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+BLUE='\033[0;34m'                                       #blue 
+RED='\033[0;31m'                                        #red 
+ORNG='\033[0;33m'                                       #orange 
+NC='\033[0m'                                            #no colour 
+
 check_number()
 {
 NUM=$x
@@ -9,32 +14,30 @@ while true; read NUM
         do                                                      #check if the input is numerical
                 echo "$NUM" | grep -iq [a-z] || echo "$NUM" | grep -q '[[:punct:]]'     # \\check letters and special characters 
                 if test $? -eq 0 ; then
-                        echo "This is not a number! Try again "
+                        echo -e "${RED}This is not a number! Try again ${NC}"
                 continue                                        #go back to: enter input
 
-                else 
+                else
 
-		NUM=$((10#$NUM))               #force number with leading zero into decimal
+                NUM=$((10#$NUM))               #force number with leading zero into decimal
 
                 fi
                 break
         done
 }
 
-echo "Enter number A: "
+echo -e "\n${ORNG}Enter number A: ${NC}"
 check_number $x
 numA=$NUM
 
-echo "Enter number B: "
+echo -e "\n${ORNG}Enter number B: ${NC}"
 check_number $x
 numB=$NUM
 
 if [ "$numA" -gt "$numB" ]; then                        #if A is greater than B output True, otherwise output False
         diff=`expr $numA - $numB`
-        echo "Number A is bigger than B by $diff"
+        echo -e "\n${BLUE}Number A is bigger than B by $diff${NC}\n"
 else
         diff=`expr $numB - $numA`
-        echo "Number B is bigger than A by $diff"
+        echo -e "\n${BLUE}Number B is bigger than A by $diff${NC}\n"
 fi
-
-
